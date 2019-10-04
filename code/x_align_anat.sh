63a2e817fd1fde1ca253012dd928d0848687cec7
Tin <xutingxt@gmail.com>
2019-10-03 20:07:30 -0400

    add basic code

---
+++ code/x_align_anat.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Align anatomical image to a template.
+# usuage: x_align_anat.sh <TemplateHead> <TemplateBrain> <TemplateMask> <Anat> <AnatMask>
+#
+# Lei.Ai, Ting.Xu @ Child Mind Institute
+
+
+# figure generation.
+vcheck (){
+    # this overlay oen image to another and save 3 slices into one figure.
+    overlay=$1
+    underlay=$2
+    figout=$3
+    workdir=`dirname ${figout}`
+    pushd ${workdir}
+    minImage=`fslstats ${overlay} -P 10`
+    maxImage=`fslstats ${overlay} -P 90`
+    slicer ${overlay} ${underlay} -i ${minImage} ${maxImage} -s 2 -x 0.60 sl1.png -y 0.60 sl2.png -z 0.65 sl3.png
+    pngappend sl1.png + sl2.png + sl3.png ${figout}
+    rm sl?.png
+    popd
+}
+
+
+# Check arguments
+if [ $# != 5 ]; then
+    echo "Your input doesn't contains enough arguments"
+else
+
+
+    # template image
+    Template_head=$1
+    Template_brain=$2
+    Template_mask=$3
+
+    # anatomicla images and funcitonal image
+    anat=$4
+    anat_mask=$5
+
+    workingdir=$(dirname $anat)
+
+    pushd ${workingdir}
+
+    # get anat brain
+    if [[ -f $anat ]] && [[ -f $anat_mask ]];then
+        anat_brain=${anat_mask/mask/brain}
+        fslmaths $anat -mul $anat_mask $anat_brain
+    fi
+
+    # do anatmicla registration.
+    if [[ -f $anat_brain ]];then
+        cp $anat_brain brain.nii.gz
+        anat_brain=brain.nii.gz
+        if [[ -f $anat_mask ]];then
+            cp $anat_mask mask.nii.gz
+            anat_mask=mask.nii.gz
+        else
+            fslmaths brain.nii.gz -bin mask.nii.gz
+        fi
+
+
+        ls $Template_mask
+        mri_nu_correct.mni --i $anat_brain --o brain_nu.nii.gz  --n 6 --proto-iters 150 --stop .0001;
+        N4BiasFieldCorrection -d 3 -i brain_nu.nii.gz -o brain_nuN4.nii.gz;
+        flirt -ref $Template_brain -in brain_nuN4.nii.gz -omat anat2Template.mat
+        echo "Doing fnirt, may take a little while"
+        fnirt --in=brain_nuN4.nii.gz --aff=anat2Template.mat --cout=anat2Template_warp --iout=fnirt_anat2Template --jout=anat2Template_jac --ref=$Template_head --refmask=$Template_mask --warpres=10,10,10
+        vcheck $Template_brain fnirt_anat2Template.nii.gz $workingdir'/anat_align.png'
+    fi
+
+    popd
+
+fi
---
+++ code/x_align_func.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Align functional image to a template, it uses a warp file created form the anatomy to template in fnirt.
+# usuage: x_align_func.sh <TemplateBrain> <Func> <AnatBrain> <anat2template_warp>
+#
+# Lei.Ai, Ting.Xu @ Child Mind Institute
+
+# figure generation function.
+vcheck (){
+    # this overlay oen image to another and save 3 slices into one figure.
+    overlay=$1
+    underlay=$2
+    figout=$3
+    workdir=`dirname ${figout}`
+    pushd ${workdir}
+    minImage=`fslstats ${overlay} -P 10`
+    maxImage=`fslstats ${overlay} -P 90`
+    slicer ${overlay} ${underlay} -i ${minImage} ${maxImage} -s 2 -x 0.60 sl1.png -y 0.60 sl2.png -z 0.65 sl3.png
+    pngappend sl1.png + sl2.png + sl3.png ${figout}
+    rm sl?.png
+    popd
+}
+
+
+
+# Check arguments
+if [ $# != 4 ]; then
+    echo "Your input doesn't contains enough arguments"
+else
+
+    # template image
+    Template_brain=$1
+
+
+    # anatomicla images and funcitonal image
+    func=$2
+    anat_brain=$3
+    anat2template_warp=$4
+
+    workingdir=$(dirname $func)
+
+    pushd ${workingdir}
+
+    cp $func func.nii.gz
+    fslmaths $anat_brain -bin anat_mask.nii.gz
+    
+    # func
+    if [[ -f "func.nii.gz" ]] && [[ -f "$anat2template_warp" ]] && [[ -f "anat_mask.nii.gz" ]];then
+        nt=$(3dinfo -ntimes func.nii.gz)
+        fslroi func.nii.gz example_func $((nt/2)) 1
+        N4BiasFieldCorrection -d 3 -i example_func.nii.gz -o example_func_N4.nii.gz;
+
+        # func mask from bet2 and 3dAutomask
+        3dAutomask -prefix example_func_N4_3dautomask.nii.gz example_func_N4.nii.gz
+        #3dAFNItoNIFTI -prefix example_func_N4_3dautomask.nii.gz example_func_N4_3dautomask+orig 
+        bet2 example_func_N4.nii.gz example_func_N4_bet2mask.nii.gz
+        fslmaths example_func_N4_bet2mask.nii.gz -mul example_func_N4_3dautomask.nii.gz -bin combine_func_mask.nii.gz
+        fslmaths combine_func_mask.nii.gz -mul example_func_N4.nii.gz example_func_N4_brain_init.nii.gz
+
+        # anatomical normalization
+        mri_nu_correct.mni --i $anat_brain --o brain_nu.nii.gz  --n 6 --proto-iters 150 --stop .0001;
+        N4BiasFieldCorrection -d 3 -i brain_nu.nii.gz -o brain_nuN4.nii.gz;
+
+        # registration
+        flirt -dof 6 -in example_func_N4_brain_init.nii.gz -ref brain_nuN4.nii.gz -omat func_2_T1w_init.mat -out example_func__N4_2_t1w_init.nii.gz
+        convert_xfm -inverse -omat T1w_2_func.mat func_2_T1w_init.mat
+        flirt -applyxfm -init T1w_2_func.mat -in anat_mask.nii.gz -interp nearestneighbour -out func_mask_refine.nii.gz -ref example_func_N4_brain_init.nii.gz
+        fslmaths func_mask_refine.nii.gz -mul example_func_N4.nii.gz example_func_N4_brain_refine.nii.gz
+        #flirt -applyxfm -init func_2_T1w_init.mat -in example_func_brain_refine.nii.gz -ref brain.nii.gz -out example_func_2_t1w_refine.nii.gz
+        flirt -dof 6 -in example_func_N4_brain_refine.nii.gz -ref brain.nii.gz -omat func_2_T1w_refine.mat -out example_func_N4_2_t1w_refine.nii.gz
+        fslmaths example_func_N4_2_t1w_refine.nii.gz -bin func_mask_refine_refine.nii.gz
+
+        fslmaths example_func_N4.nii.gz -mul func_mask_refine.nii.gz example_func_N4_brain_refine_refine.nii.gz
+        #flirt -applyxfm -init func_2_T1w_refine.mat -in func_brain.nii.gz -ref brain.nii.gz -out func_2_t1w.nii.gz
+
+        applywarp --ref=$Template_brain --in=example_func_N4_brain_refine_refine.nii.gz --out=example_func2Template.nii.gz --warp=T1w2Yerkes19_warp.nii.gz --premat=func_2_T1w_refine.mat
+
+        #fslmaths func_warp.nii.gz -Tmean func_warp_tmean.nii.gz
+        vcheck example_func2Template.nii.gz $Template_brain $workingdir"/fucn_align.png"
+
+    fi
+    popd
+fi
+
